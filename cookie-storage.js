14d1d4df3db480769869bf6f3925d1832fe729f5
İsmail Ceylan <ismaillceylan@gmail.com>
2024-06-13 21:30:46 +0300

    fix: add path to remove cookies
    Cookies wasn't deleted on chrome, adding path solved the problem.

--- cookie-storage.js
+++ cookie-storage.js
@@ -85,7 +85,7 @@
 			return;
 		}
 		
-		document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+		document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
 		
 		delete this[ name ];
 
